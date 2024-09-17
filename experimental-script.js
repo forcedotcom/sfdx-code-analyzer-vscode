c5c63ce0c3349f8ab4eb503d64882bc4a562824b
Joshua Feingold <jfeingold35@gmail.com>
2024-09-17 15:42:52 -0500

    asdfasdfasdf

--- experimental-script.js
+++ experimental-script.js
@@ -6,12 +6,10 @@ cp.on('close', code => {
 	console.log(`isSfCliInstalled got close event, code is ${code}`);
 	// If the exit code is 0, then SF or SFDX is present.
 	// Otherwise, it's not.
-	res(code === 0);
 });
 
 cp.on('exit', code => {
 	console.log(`isSfCliInstalled got exit event, code is ${code}`);
-	res(code === 0);
 })
 
 cp.on('error', err => {
