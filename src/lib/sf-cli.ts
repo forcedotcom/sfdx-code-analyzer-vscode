7bd1f38fb9bb5cc09c1f91c3409faf29ce068b80
Joshua Feingold <jfeingold35@gmail.com>
2024-09-17 14:39:54 -0500

    asdf

--- src/lib/sf-cli.ts
+++ src/lib/sf-cli.ts
@@ -20,10 +20,16 @@ export class SfCli {
             const cp = cspawn.spawn('sf', ['-v']);
 
             cp.on('close', code => {
+				console.log(`isSfCliInstalled got close event, code is ${code}`);
                 // If the exit code is 0, then SF or SFDX is present.
                 // Otherwise, it's not.
                 res(code === 0);
             });
+
+			cp.on('exit', code => {
+				console.log(`isSfCliInstalled got exit event, code is ${code}`);
+				res(code === 0);
+			})
         });
     }
 
