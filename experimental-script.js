472a52973ad5c72d801c93ff523eb41678efdfb8
Joshua Feingold <jfeingold35@gmail.com>
2024-09-17 15:34:51 -0500

    asdfasdfasdfg

---
+++ experimental-script.js
@@ -0,0 +1,19 @@
+const cp = require('child_process');
+
+cp.spawn('sf', ['-v']);
+
+cp.on('close', code => {
+	console.log(`isSfCliInstalled got close event, code is ${code}`);
+	// If the exit code is 0, then SF or SFDX is present.
+	// Otherwise, it's not.
+	res(code === 0);
+});
+
+cp.on('exit', code => {
+	console.log(`isSfCliInstalled got exit event, code is ${code}`);
+	res(code === 0);
+})
+
+cp.on('error', err => {
+	console.log(`isSfCliInstalled got error event, err is ${err.name}, ${err.message}`);
+})
