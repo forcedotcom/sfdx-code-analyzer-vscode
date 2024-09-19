4fe03117430f3389ad762534ef8f5befed201c1d
Joshua Feingold <jfeingold35@gmail.com>
2024-09-19 14:11:26 -0500

    do not merge

--- src/test/suite/issue-demonstration.test.ts
+++
@@ -1,7 +0,0 @@
-import {expect} from 'chai';
-
-suite('demoing issue', () => {
-	test('path is changed', () => {
-		expect(process.env.PATH).to.contain('/Users/runner/hostedtoolcache/node/20.17.0/arm64/bin');
-	})
-})
