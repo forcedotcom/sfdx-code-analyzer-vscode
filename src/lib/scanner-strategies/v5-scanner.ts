26b317804bba17cb7228e715786286b0f9a640bd
Joshua Feingold <jfeingold35@gmail.com>
2025-02-11 13:13:36 -0600

    Fixed maybe

--- src/lib/scanner-strategies/v5-scanner.ts
+++ src/lib/scanner-strategies/v5-scanner.ts
@@ -3,7 +3,7 @@ import { DiagnosticConvertible } from '../diagnostics';
 import {messages} from '../messages';
 import * as cspawn from 'cross-spawn';
 import { tmpFileWithCleanup } from '../file';
-import * as ansis from 'ansis';
+import {stripAnsi} from '../string-utils';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
@@ -44,9 +44,9 @@ export class CliScannerV5Strategy extends CliScannerStrategy {
 				console.log(`===\nsf plugins stdout was \n${stdout}`);
 				console.log(`code is 0? ${code === 0 ? 'yes' : 'no'}`);
 				console.log(`bytes are ${Array.from(Buffer.from(stdout)).join(' ')}`);
-				console.log(`stdout has string? ${ansis.strip(stdout).includes('code-analyzer 5.0.0-beta') ? 'yes' : 'no'}`)
-				console.log(`success with regex? ${/code-analyzer\s*5\.0\.0-beta/.test(ansis.strip(stdout)) ? 'yes' : 'no'}\n===`)
-				return res(code === 0 && stdout.includes('code-analyzer 5.0.0-beta'));
+				console.log(`stdout has string? ${stripAnsi(stdout).includes('code-analyzer 5.0.0-beta') ? 'yes' : 'no'}`)
+				console.log(`success with regex? ${/code-analyzer\s*5\.0\.0-beta/.test(stripAnsi(stdout)) ? 'yes' : 'no'}\n===`)
+				return res(code === 0 && stripAnsi(stdout).includes('code-analyzer 5.0.0-beta'));
 			});
 		});
 		if (!codeAnalyzerIsInstalled) {
