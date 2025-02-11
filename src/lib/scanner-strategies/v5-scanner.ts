c26f05def868275aa86d7ef3212d934b1b04de54
Joshua Feingold <jfeingold35@gmail.com>
2025-02-11 15:56:38 -0600

    asdfasdfasdf

--- src/lib/scanner-strategies/v5-scanner.ts
+++ src/lib/scanner-strategies/v5-scanner.ts
@@ -162,7 +162,11 @@ export class CliScannerV5Strategy extends CliScannerStrategy {
 
 		for (const violation of resultsJson.violations) {
 			for (const location of violation.locations) {
-				location.file = path.join(resultsJson.runDir, location.file);
+				// If the path isn't already absolute, it needs to be made absolute.
+				if (path.resolve(location.file).toLowerCase() !== location.file.toLowerCase()) {
+					// Relative paths are relative to the RunDir property.
+					location.file = path.join(resultsJson.runDir, location.file);
+				}
 			}
 			processedConvertibles.push(violation);
 		}
