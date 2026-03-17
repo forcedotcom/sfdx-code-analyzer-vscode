f1b1d2c74af0b6b63dfd06305b7d9e513a70ebc7
Nikhil Mittal <nik.mittal165@gmail.com>
2026-03-17 15:14:29 +0530

    Revert "fix test cases"
    This reverts commit 1e3706fdc7c3521f94be36cd78edf93d8ef5fe4d.

--- end-to-end/tests/extension.test.ts
+++ end-to-end/tests/extension.test.ts
@@ -14,10 +14,8 @@ suite('E2E Extension tests', function () {
         await vscode.commands.executeCommand('workbench.action.closeAllEditors');
     });
 
-    test('Extension should be activated (since the sampleWorkspace has sfdx-project.json file)', async () => {
+    test('Extension should be activated (since the sampleWorkspace has sfdx-project.json file)', () => {
         const extension: vscode.Extension<unknown> = vscode.extensions.getExtension('salesforce.sfdx-code-analyzer-vscode');
-        // Await activate() to ensure the async activation fully completes and all commands are registered
-        await extension.activate();
         expect(extension.isActive).equals(true);
     });
 
